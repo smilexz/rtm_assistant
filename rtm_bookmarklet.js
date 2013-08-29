36b8351f6e79552b7cf8b71d158cfbd693e382e8
smilexz <smilexzj@gmail.com>
2013-08-29 21:16:08 +0700

    delet

--- rtm_bookmarklet.js
+++
@@ -1,3 +0,0 @@
-//http://www.rememberthemilk.com/forums/tips/3133/
-
-javascript:(function(){h='m.rememberthemilk.com';p='/add';if(window.getSelection){t=window.getSelection();}else%20if(document.getSelection){t=document.getSelection();}else%20if(document.selection){t=document.selection.createRange().text;};cp='http://'+h+p+'?name='+((null==t||t==""||t==undefined)?encodeURIComponent(document.title):t)+'&url='+encodeURIComponent(location.href);w=window.open(cp,'addwindow','status=no,toolbar=no,width=250,height=560,resizable=yes');setTimeout(function(){w.focus();},500);})();
